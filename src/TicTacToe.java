6a3df279ccae3e8ad664d71d1c87cd7b438f258e
Zaria <91369177+zariafk@users.noreply.github.com>
2024-04-01 22:35:52 +0100

    First Commit

---
+++ src/TicTacToe.java
@@ -0,0 +1,74 @@
+import java.util.*;
+
+public class TicTacToe {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Tic Tac Toe!");
+
+        int[] positions = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        //enterPos();
+        
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter a position: ");
+        int pos = keyboard.nextInt();
+        List<Integer> player = new ArrayList<>();
+        }
+
+    /* 
+    public static int enterPos() {
+        int pos;
+        try {
+            Scanner keyboard = new Scanner(System.in);
+            System.out.println("Enter a position: ");
+            pos = keyboard.nextInt();
+        }
+        catch (Exception e) {
+            System.out.println("Error: an integer was not entered");
+        }
+        return pos;
+    }
+    */
+
+    public static void checkPos(int pos) {
+        List<Integer> player = new ArrayList<>();
+        switch (pos) {
+            case 1 : 
+                player.add(1);
+                break;
+            case 2 : 
+                player.add(2);
+                break;
+            case 3 :
+                player.add(3);
+                break;
+            case 4 :
+                player.add(5);
+                break;
+            case 5 :
+                player.add(6);
+                break;
+            case 6 :
+                player.add(6);
+                break;
+            case 7 : 
+                player.add(7);
+                break;
+            case 8 :
+                player.add(8);
+                break;
+            case 9 :
+                player.add(9);
+                break;
+            default :
+                myError();
+        }
+    }
+
+    public static void myError() {
+        try {
+            throw new Exception ("input out of range");
+        }
+        catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
