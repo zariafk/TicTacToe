e0f5df36f3c175e1269e2b3eed4263a7b33aaf45
Zaria <91369177+zariafk@users.noreply.github.com>
2024-04-05 23:55:41 +0100

    Pos Error Handling

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -2,68 +2,82 @@ import java.util.*;
 
 public class TicTacToe {
     public static void main(String[] args) {
+        // VARIABLES
+        int[] positions = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        List<Integer> player;
+        int pos;
+
+        // WELCOME MESSAGE
         System.out.println("Welcome to Tic Tac Toe!");
 
-        int[] positions = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        //enterPos();
-        
+        // GETTING POSITION INPUT FROM USER
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("Enter a position: ");
-        int pos = keyboard.nextInt();
-        List<Integer> player = new ArrayList<>();
-        checkPos(pos, player);
+        player = new ArrayList<>();
+        pos = enterPos(player);
+        
+        // KEEPS ASKING FOR INPUT UNTIL VALID INPUT IS ENTERED
+        while (pos == -1) {
+            pos = enterPos(player);
         }
 
-    /* 
-    public static int enterPos() {
+        keyboard.close();
+    }
+
+    
+    public static int enterPos(List<Integer> player) {
         int pos;
         try {
             Scanner keyboard = new Scanner(System.in);
             System.out.println("Enter a position: ");
             pos = keyboard.nextInt();
+            pos = checkPos(pos, player);
+            return pos;
         }
+        // WHEN A DATA TYPE THAT IS NOT AN INTEGER IS ENTERED-ERROR HANDLING
         catch (Exception e) {
             System.out.println("Error: an integer was not entered");
         }
-        return pos;
+        return -1;
     }
-    */
+    
 
-    public static void checkPos(int pos, List<Integer> player) {
+    public static int checkPos(int pos, List<Integer> player) {
         switch (pos) {
             case 1 : 
                 player.add(1);
-                break;
+                return 1;
             case 2 : 
                 player.add(2);
-                break;
+                return 2;
             case 3 :
                 player.add(3);
-                break;
+                return 3;
             case 4 :
                 player.add(5);
-                break;
+                return 4;
             case 5 :
                 player.add(6);
-                break;
+                return 5;
             case 6 :
                 player.add(6);
-                break;
+                return 6;
             case 7 : 
                 player.add(7);
-                break;
+                return 7;
             case 8 :
                 player.add(8);
-                break;
+                return 8;
             case 9 :
                 player.add(9);
-                break;
+                return 9;
             default :
-                myError();
+                rangeError();
+                return -1;
         }
     }
 
-    public static void myError() {
+    // WHEN AN INTEGER THAT IS NOT A POSITION ON THE GAME BOARD IS ENTERED-ERROR HANDLING
+    public static void rangeError() {
         try {
             throw new Exception ("input out of range");
         }
